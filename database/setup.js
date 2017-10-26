1b1b22dea449d357dbf8fb6a9b5c0e611b35b39b
David Friedman <dfried514@gmail.com>
2017-10-25 19:14:51 -0700

    set up database schema

--- database/setup.js
+++ database/setup.js
@@ -3,7 +3,8 @@ const config = require('./config');
 
 const pool = new Pool(config);
 
-pool.query('CREATE TYPE genre AS ENUM ('action', 'action_adventure', 'adventure', 'role_playing', 'simulation', 'strategy', 'sports'))
+pool.query("CREATE TYPE genre AS ENUM ('action', 'action_adventure', 'adventure', 'role_playing', 'simulation', 'strategy', 'sports')")
+
 .then(() =>
 
-  pool.query('CREATE TABLE IF NOT EXISTS games (game_id BIGSERIAL UNIQUE NOT NULL PRIMARY KEY, name TEXT UNIQUE NOT NULL, publisher TEXT, release_date DATE, genre_type genre, price MONEY, number_of_owners BIGINT'));
+  pool.query('CREATE TABLE games (game_id BIGSERIAL UNIQUE NOT NULL PRIMARY KEY, name TEXT UNIQUE NOT NULL, publisher TEXT, release_date DATE, genre_type genre, price MONEY, number_of_owners BIGINT)'));
